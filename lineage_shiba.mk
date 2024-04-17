2454c27e850bda4cb648eae59efd32ffb1a51583
Brian <trackingadeadguy@gmail.com>
2024-04-16 21:03:24 -0400

    Matrixx-ify

--- lineage_shiba.mk
+++ lineage_shiba.mk
@@ -14,13 +14,26 @@ $(call inherit-product, device/google/zuma/lineage_common.mk)
 
 include device/google/shusky/shiba/device-lineage.mk
 
+
 # Xtras
 TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 WITH_GMS := true
+TARGET_PIXEL_BOOT_ANIMATION_RES := 1440
 TARGET_IS_PIXEL_8 := true
 TARGET_IS_PIXEL := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_HAS_UDFPS := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
+
+#matrixx Settings
+MATRIXX_MAINTAINER := Brian_P8Pro
+MATRIXX_CHIPSET := Tensor3
+MATRIXX_BATTERY := 4575mah
+MATRIXX_DISPLAY := 2400x1080
+
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
